6b8eb340fa205c34fed72d91e0176ec9312a88d4
Krishna Dubagunta <dubagunta.saikrishna@outlook.com>
2017-06-15 01:00:12 -0400

    Folder Rearrangement

---
+++ OnboardingSample/OnboardingSample/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  OnboardingSample
+//
+//  Created by Sai Krishna Dubagunta on 6/14/17.
+//  Copyright © 2017 Sai Krishna Dubagunta. All rights reserved.
+//
+
+import UIKit
+import Material
+
+class ViewController: UIViewController {
+    
+    let pageView1 = OnboardingView(frame: UIScreen.main.bounds)
+    let pageView2 = OnboardingView(frame: UIScreen.main.bounds)
+    let pageView3 = OnboardingView(frame: UIScreen.main.bounds)
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+extension ViewController {
+    fileprivate func preparePages() {
+    }
+}
+
